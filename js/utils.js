d8f1b70592c8d4a8df2ed04cdd8cd876e2f98936
Alexander Bihary <alex.bihary@gmail.com>
2013-12-30 23:45:51 -0500

    initial page preview and js

---
+++ js/utils.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function(window, document, $, namespace, undefined) {
+
+})(window, document, jQuery || Zepto, '_pergola', function(window, document, namespace, undefined) {
+
+  var asd = { a: 1 };
+
+  window[namespace] = asd;
+});
